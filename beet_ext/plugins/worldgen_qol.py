cfb7400456f5f50bf78120ed670859aa2085ea09
Arcensoth <arcensoth@gmail.com>
2022-02-22 00:52:54 -0500

    Automatically correct template pool names

--- beet_ext/plugins/worldgen_qol.py
+++ beet_ext/plugins/worldgen_qol.py
@@ -2,10 +2,12 @@ import logging
 
 from beet import Context
 
+log = logging.getLogger(__name__)
+
 
 def beet_default(ctx: Context):
-    logger = logging.getLogger("livereload")
-    logger.addFilter(
+    livereload_logger = logging.getLogger("livereload")
+    livereload_logger.addFilter(
         lambda r: not r.getMessage().startswith(
             "Creating a MIN function between two non-overlapping inputs"
         )
@@ -19,3 +21,8 @@ def beet_default(ctx: Context):
                         rule["location_predicate"] = {
                             "predicate_type": "minecraft:always_true"
                         }
+
+    for name, template_pool in ctx.data.template_pools.items():
+        if template_pool.data["name"] != name:
+            log.warning(f"Correcting template pool name mismatch in: {name}")
+            template_pool.data["name"] = name
