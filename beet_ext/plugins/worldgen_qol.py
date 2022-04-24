4eab8a30fecda547786c760f45e016941735ae2e
Arcensoth <arcensoth@gmail.com>
2022-04-24 17:09:27 -0400

    Update beet and mecha

--- beet_ext/plugins/worldgen_qol.py
+++ beet_ext/plugins/worldgen_qol.py
@@ -1,6 +1,7 @@
 import logging
 
 from beet import Context
+from beet.contrib.worldgen import WorldgenProcessorList, WorldgenTemplatePool
 
 log = logging.getLogger(__name__)
 
@@ -13,7 +14,7 @@ def beet_default(ctx: Context):
         )
     )
 
-    for processor_list in ctx.data.processor_lists.values():
+    for processor_list in ctx.data[WorldgenProcessorList].values():
         for processor in processor_list.data["processors"]:
             if processor["processor_type"] == "minecraft:rule":
                 for rule in processor["rules"]:
@@ -22,7 +23,7 @@ def beet_default(ctx: Context):
                             "predicate_type": "minecraft:always_true"
                         }
 
-    for name, template_pool in ctx.data.template_pools.items():
+    for name, template_pool in ctx.data[WorldgenTemplatePool].items():
         if template_pool.data["name"] != name:
             log.warning(f"Correcting template pool name mismatch in: {name}")
             template_pool.data["name"] = name
